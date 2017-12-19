35baade050f3e1381ac27d8002026ceb709ad5fb
Dorian Delorme <dorian.delorme@hetic.net>
2017-12-19 15:00:20 +0100

    Add SearchViewController

---
+++ panpan/SearchViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SearchViewController.swift
+//  panpan
+//
+//  Created by Dorian Delorme on 19/12/2017.
+//  Copyright © 2017 Dorian Delorme. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+import FacebookCore
+import FirebaseAuth
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var DepartureTextField: UITextField!
+    @IBOutlet weak var DestinationTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+}
